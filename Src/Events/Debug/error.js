d38d011e6b18e2b0bf445589b9493576151496cb
ShadowoftheRedFox <74469156+ShadowoftheRedFox@users.noreply.github.com>
2022-10-21 12:59:18 +0200

    Delete Src directory

--- Src/Events/Debug/error.js
+++
@@ -1,6 +0,0 @@
-const Event = require('../../Structures/Event');
-module.exports = class extends Event {
-    async run(error) {
-        return console.log(error.stack);
-    }
-};
--- Src/Events/Debug/warn.js
+++
@@ -1,8 +0,0 @@
-const Event = require('../../Structures/Event');
-var colors = require("colors");
-
-module.exports = class extends Event {
-    async run(string) {
-        return console.log(["WARN".bgYellow.black, string].join(" "));
-    }
-};
